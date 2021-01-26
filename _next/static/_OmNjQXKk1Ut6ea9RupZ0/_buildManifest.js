8116fab66b1eeb21e62466a50cd0127a9e59f5b8
sadpvndv <andreseloy08@gmail.com>
2021-01-26 22:52:13 +0000

    Deploying to gh-pages from  @ 62ee082425c757498ea7b6bc2a580d6429b7b351 🚀

---
+++ _next/static/_OmNjQXKk1Ut6ea9RupZ0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.232829eabd025e8acd51.js","static\u002Fchunks\u002Fpages\u002Findex-e43ce72ececd22dc1d94.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2157553d569a4a4e7bf2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/qvrHceHGE4g5ypD6x0X4t/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.232829eabd025e8acd51.js","static\u002Fchunks\u002Fpages\u002Findex-8ec9d6767f996d9c1e7a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2157553d569a4a4e7bf2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
