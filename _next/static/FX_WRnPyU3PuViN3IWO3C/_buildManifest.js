224507f7262e2748ce9ed91c44ea0ac74da275eb
sadpvndv <andreseloy08@gmail.com>
2021-01-26 00:56:48 +0000

    Deploying to gh-pages from  @ cb42af94cefd294dd4825521caaf96cfa8fc71e3 🚀

--- _next/static/FX_WRnPyU3PuViN3IWO3C/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.45553b1c249ce8eda964.js","static\u002Fchunks\u002Fpages\u002Findex-288ee15c6da7a3b5afb1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2157553d569a4a4e7bf2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
